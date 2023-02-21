23710d60e5d983c57e2c23e9b4728fd271f29809
drizzle0171 <yongsandew@gmail.com>
2023-02-21 17:46:41 +0900

    update code

--- Image-captioning/crawling/crawling.py
+++ Image-captioning/crawling/crawling.py
@@ -1,50 +1,46 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 from selenium.webdriver.common.keys import Keys
 import urllib.request
 import time
 import os
 
-driver = webdriver.Chrome(executable_path = r'/nas/home/drizzle0171/Solution-Challenge-2023/Image-captioning/crawling/chromedriver') 
+driver = webdriver.Chrome(executable_path = r'./chromedriver') 
 
 keyword = str(input("insert keyword for searching : "))
-path = os.path.join(keword)
+path = os.path.join('./', keyword)
 if not os.path.exists(path):
     os.makedirs(path)
 
 driver.get("https://www.google.co.kr/imghp?hl=ko&authuser=0&ogbl")
 driver.maximize_window()
 time.sleep(2)
-driver.find_element_by_css_selector("input.gLFyf").send_keys(keyword) 
-driver.find_element_by_css_selector("input.gLFyf").send_keys(Keys.RETURN)
+driver.find_element(By.CSS_SELECTOR, "input.gLFyf").send_keys(keyword) 
+driver.find_element(By.CSS_SELECTOR,"input.gLFyf").send_keys(Keys.RETURN)
 
 
-last_height = driver.execute_script("return document.body.scrollHeight")
-while True:
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(1)
-    new_height = driver.execute_script("return document.body.scrollHeight")
-    if new_height == last_height:
-        try:
-            driver.find_element_by_css_selector(".mye4qd").click()
-        except:
-            break
-    last_height = new_height
+# last_height = driver.execute_script("return document.body.scrollHeight")
+# while True:
+#     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+#     time.sleep(1)
+#     new_height = driver.execute_script("return document.body.scrollHeight")
+#     if new_height == last_height:
+#         try:
+#             driver.find_element(By.CSS_SELECTOR,".mye4qd").click()
+#         except:
+#             break
+#     last_height = new_height
 
-i=0
+i=1
 
-list = driver.find_elements_by_css_selector("img.rg_i.Q4LuWd")
-print(len(list))
-
-
+imgs = driver.find_elements(By.CSS_SELECTOR,"img.rg_i.Q4LuWd")
 address = (f'./{keyword}')
-for img in list:
+for img in imgs:
+    if i == 101:
+        break
+    imgurl = img.get_attribute("src")
+    time.sleep(1)
+    urllib.request.urlretrieve(imgurl,address+"/"+str(keyword)+str(i)+".jpeg")
     i += 1
-    try:
-
-        imgurl = img.get_attribute("src")
-        time.sleep(1)
-        urllib.request.urlretrieve(imgurl,address+"/"+str(keyword)+str(i)+".jpeg")
-
-    except:
-        pass
+    print(i)
