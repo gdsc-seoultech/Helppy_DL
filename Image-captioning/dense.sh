93c8a8e6a1ed87c5d821802889dcb11105a1ac95
drizzle0171 <yongsandew@gmail.com>
2023-02-21 16:20:17 +0900

    initial commit

---
+++ Image-captioning/dense.sh
@@ -0,0 +1,5 @@
+python describe.py \
+    --config_json ./config.json \
+    --model_checkpoint ./model_params/train_all_val_all_bz_2_epoch_10_inject_init.pth.tar \
+    --img_path ./images/jb.jpeg \
+    --result_dir ./result 
