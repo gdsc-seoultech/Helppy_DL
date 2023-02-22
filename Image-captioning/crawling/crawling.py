383383412f78b508b4d5afcbe0fd2731bb5321b1
drizzle0171 <yongsandew@gmail.com>
2023-02-22 21:52:01 +0900

    raw data

--- Image-captioning/crawling/crawling.py
+++ Image-captioning/crawling/crawling.py
@@ -6,13 +6,12 @@ import urllib.request
 import time
 import os
 
-driver = webdriver.Chrome(executable_path = r'./chromedriver') 
-
 keyword = str(input("insert keyword for searching : "))
-path = os.path.join('./', keyword)
+path = os.path.join('./crawling/data/', keyword)
 if not os.path.exists(path):
     os.makedirs(path)
 
+driver = webdriver.Chrome(executable_path = r'./chromedriver') 
 driver.get("https://www.google.co.kr/imghp?hl=ko&authuser=0&ogbl")
 driver.maximize_window()
 time.sleep(2)
@@ -20,27 +19,27 @@ driver.find_element(By.CSS_SELECTOR, "input.gLFyf").send_keys(keyword)
 driver.find_element(By.CSS_SELECTOR,"input.gLFyf").send_keys(Keys.RETURN)
 
 
-# last_height = driver.execute_script("return document.body.scrollHeight")
-# while True:
-#     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-#     time.sleep(1)
-#     new_height = driver.execute_script("return document.body.scrollHeight")
-#     if new_height == last_height:
-#         try:
-#             driver.find_element(By.CSS_SELECTOR,".mye4qd").click()
-#         except:
-#             break
-#     last_height = new_height
-
-i=1
-
-imgs = driver.find_elements(By.CSS_SELECTOR,"img.rg_i.Q4LuWd")
-address = (f'./{keyword}')
-for img in imgs:
-    if i == 101:
-        break
-    imgurl = img.get_attribute("src")
+last_height = driver.execute_script("return document.body.scrollHeight")
+while True:
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     time.sleep(1)
-    urllib.request.urlretrieve(imgurl,address+"/"+str(keyword)+str(i)+".jpeg")
-    i += 1
-    print(i)
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        try: break
+        except : break
+    last_height = new_height
+i=1
+imgs = driver.find_elements(By.CSS_SELECTOR,"img.rg_i.Q4LuWd")
+address = (f'./crawling/data/{keyword}')
+print(len(imgs))
+for img in imgs:
+    try:
+        print(i)
+        if i == 101:
+            break
+        imgurl = img.get_attribute("src")
+        time.sleep(1)
+        urllib.request.urlretrieve(imgurl,address+"/"+str(keyword)+str(i)+".jpeg")
+        i += 1
+    except:
+        pass
